d81aaeffa7c2e22c079e1b2f54e28ba5777c6a3d
Grace Brown <gracenbrown@sbcglobal.net>
2021-04-10 20:53:38 -0500

    Created header, footer, and trivia component. Edited HTML and CSS.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TriviaComponent } from './trivia/trivia.component';
+import { Route, RouterModule } from '@angular/router';
+
+const routes: Route[] = [
+  {path: 'trivia', component: TriviaComponent},
+  {path: '', component: TriviaComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TriviaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
